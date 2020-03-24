70fdd91c10f935172497f5c40146057d2d18580c
manish106 <parmarmanishj2012@gmail.com>
2020-03-24 14:54:19 +0530

    problem 1

---
+++ AnalysisProblem1/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnalysisProblem1
+{
+    class User
+    {
+
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public int MobileNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
