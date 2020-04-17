c760380c8a49f3295e6872c3eb180813c3f5cd6b
manish106 <parmarmanishj2012@gmail.com>
2020-04-17 15:09:57 +0530

    trigger added

--- LabExercise/InsteadOfTrigger.sql
+++ LabExercise/InsteadOfTrigger.sql
@@ -1,22 +1,31 @@
 
-ALTER TRIGGER TR_Update
-ON vUpdate
+
+
+
+Create view vStudentDetails
+as select Students.StudentId,FirstName,LastName,Email,MobileNumber from Students,StudentsDetails
+where Students.StudentId=StudentsDetails.StudentId
+
+select *from vStudentDetails
+
+
+CREATE TRIGGER TR_StudentUpdate
+ON vStudentDetails
 INSTEAD OF Update
 AS
 SET NOCOUNT ON;
 declare @id int
-declare  @sid int
 declare @FirstName varchar(50)
 declare @lastname varchar(50)
 declare @Email varchar(50)
 declare @MobileNumber int
-select @id=StudentId,@sid=Id, @FirstName=FirstName,@lastname=LastName,@Email=Email,@MobileNumber=MobileNumber  from inserted
-update vUpdate set FirstName=@FirstName,LastName=@lastname where StudentId=@id
-update vUpdate set Email=@Email,MobileNumber=@MobileNumber where Id=@sid
+select @id=StudentId,@FirstName=FirstName,@lastname=LastName,@Email=Email,@MobileNumber=MobileNumber  from inserted
+update vStudentDetails set FirstName=@FirstName,LastName=@lastname where StudentId=@id
+update vStudentDetails set Email=@Email,MobileNumber=@MobileNumber where StudentId=@id
 
 GO
 
-update vUpdate set firstname='Manish',mobilenumber=98252 where studentid=1
+update vStudentDetails set firstname='Manish',mobilenumber=9825212 where studentid=1
 select *from Students
 select *from StudentsDetails
-select *from vupdate
+select *from vStudentDetails
