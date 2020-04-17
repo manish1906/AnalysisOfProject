528f8f4f3fd9167996edddcceefea09926cc09dc
manish106 <parmarmanishj2012@gmail.com>
2020-04-17 10:58:28 +0530

    Module 11 updated

--- LabExercise/Module 11/Module 11 Exercise 2.sql
+++ LabExercise/Module 11/Module 11 Exercise 2.sql
@@ -43,3 +43,33 @@ WHERE ProductID=760;
 
 SELECT * FROM Production.ProductAudit
 GO
+
+
+
+--------------------------------------------------------------------------
+
+
+USE MarketDev
+GO
+
+ALTER TRIGGER Marketing.TR_CampaignBalance_Update
+ON Marketing.CampaignBalance
+AFTER UPDATE
+AS BEGIN
+SET NOCOUNT ON;
+
+INSERT Marketing.CampaignAudit 
+(AuditTime, ModifyingUser, RemainingBalance)
+SELECT SYSDATETIME(),
+ORIGINAL_LOGIN(),
+inserted.RemainingBalance
+FROM deleted
+INNER JOIN inserted
+ON deleted.CampaignID = inserted.CampaignID 
+WHERE ABS(deleted.RemainingBalance  - inserted.RemainingBalance) > 10000;
+END;
+GO
+select *from Marketing.CampaignAudit
+delete from Marketing.CampaignAudit
+select *from Marketing.CampaignBalance
+UPDATE Marketing.CampaignBalance SET RemainingBalance = 20000 WHERE CampaignID=3;
